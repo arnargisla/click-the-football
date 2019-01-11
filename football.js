7a56bed402d196cc68822a3248aeb098097352d8
arnargisla <s171941@student.dtu.dk>
2019-01-11 16:55:16 +0000

    Enable user to edit ball logic

--- football.js
+++ football.js
@@ -1,123 +1,172 @@
 document.addEventListener("DOMContentLoaded", main);
-		
-		function main(){
-			const canvas = document.createElement("canvas");
-			const counter = document.createElement("div");
-			counter.innerText = 0;
-			counter.style = "position:absolute; top: 20px; left: 30px; color: white; user-select: none; font-size: 3em;";
-			const body = document.querySelector("body");
 
-			canvas.height = 1024;
-			canvas.width = 512;
+function main() {
+    const canvas = document.createElement("canvas");
+    const counter = document.createElement("div");
+    counter.innerText = 0;
+    counter.style = "position:absolute; top: 20px; left: 30px; color: white; user-select: none; font-size: 3em;";
+    const body = document.querySelector("body");
+    codearea = document.createElement("textarea");
+    codearea.rows = 40;
+    codearea.cols = 100;
+    const submitCodeButton = document.createElement("button");
+    submitCodeButton.innerText = "Submit code";
 
-			body.appendChild(canvas);
-			body.appendChild(counter);
+    canvas.height = 512;
+    canvas.width = 512;
 
-			const ctx = canvas.getContext("2d");
+    body.appendChild(canvas);
+    body.appendChild(counter);
+    body.appendChild(codearea);
+    body.appendChild(submitCodeButton);
 
-			function drawBall(ctx, ball) {
+    const ctx = canvas.getContext("2d");
 
-				ctx.fillStyle = "white";
-				ctx.beginPath();
-				ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2);
-				ctx.closePath();
-				ctx.fill();
+    function drawBall(ctx, ball) {
 
-			}
+        ctx.fillStyle = "white";
+        ctx.beginPath();
+        ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
 
-			const ball = {
-				x: canvas.width/2,
-				y: canvas.height - 50,
-				vx: 0, 
-				vy: -750,
-				ax: 0,
-				ay: 1500,
-				radius: 50,
-			}
+    }
 
-			function clickInsideBall() {
-				const dx = ball.x - click.x;
-				const dy = ball.y - click.y;
-				const r = ball.radius;
-				if(dx*dx + dy*dy < r*r){
-					return true;
-				}
-			}
+    let ball = {
+        x: canvas.width / 2,
+        y: canvas.height - 50,
+        vx: 0,
+        vy: -750,
+        ax: 0,
+        ay: 1500,
+        radius: 50,
+        update: dt=>{
+            // collide with wall
+            if (ball.x - ball.radius < 0 || ball.x + ball.radius > canvas.width) {
+                ball.vx *= -1;
+            }
 
-			function update(dt) {
+            // handle click
+            if (click.active && clickInsideBall()) {
+                console.log("hit");
+                const dx = click.x - ball.x;
+                const a = dx / ball.radius;
+                ball.vx += -a * 300;
+                ball.vy = -750 - Math.random() * 200;
 
-				// collide with wall
-				if(ball.x - ball.radius < 0 || ball.x + ball.radius > canvas.width) {
-					ball.vx *= -1;
+                counter.innerText = ++count;
+            } else if (click.active) {
+                console.log("miss");
+            }
 
-				}
+            // update physics
+            ball.vx += ball.ax * dt;
+            ball.vy += ball.ay * dt;
 
-				// handle click
-				if (click.active && clickInsideBall()) {
-					console.log("hit");
-					const dx = click.x - ball.x;
-					const a = dx/ball.radius;
-					ball.vx += -a*300;
-					ball.vy = -750-Math.random()*200;
-					
-					counter.innerText = ++count;
-				} else if (click.active){
-					console.log("miss");
-				}
+            ball.x += ball.vx * dt;
+            ball.y += ball.vy * dt;
+        },
+    };
 
+    codearea.value = 
+`ball = {
+    x: canvas.width / 2,
+    y: canvas.height - 50,
+    vx: 0,
+    vy: -750,
+    ax: 0,
+    ay: 1500,
+    radius: 50,
+    update: dt=>{
+        // collide with wall
+        if (ball.x - ball.radius < 0 || ball.x + ball.radius > canvas.width) {
+            ball.vx *= -1;
+        }
 
-				// update physics
-				ball.vx += ball.ax * dt;
-				ball.vy += ball.ay * dt;
+        // handle click
+        if (click.active && clickInsideBall()) {
+            console.log("hit");
+            const dx = click.x - ball.x;
+            const a = dx / ball.radius;
+            ball.vx += -a * 300;
+            ball.vy = -750 - Math.random() * 200;
 
-				ball.x += ball.vx * dt;
-				ball.y += ball.vy * dt;
-			}
+            counter.innerText = ++count;
+        } else if (click.active) {
+            console.log("miss");
+        }
 
-			function render(ctx) {
-				drawBall(ctx, ball);
-			}
+        // update physics
+        ball.vx += ball.ax * dt;
+        ball.vy += ball.ay * dt;
 
-			let oldTime = 0;
-			let count = 0;
-			let click = {
-				active: false,
-				x: 0,
-				y: 0,
-			}
-			function gameLoop(timestamp) {   
-				const du = Math.min(timestamp - oldTime, 30);
-				const dt = du/1000;
+        ball.x += ball.vx * dt;
+        ball.y += ball.vy * dt;
+    },
+};`;
 
-				ctx.fillStyle = "grey";
-				ctx.fillRect(0, 0, canvas.width, canvas.height);
+    submitCodeButton.addEventListener("click", e=>{
+        eval(codearea.value);
+    }
+    )
 
-				update(dt);
+    function clickInsideBall() {
+        const dx = ball.x - click.x;
+        const dy = ball.y - click.y;
+        const r = ball.radius;
+        if (dx * dx + dy * dy < r * r) {
+            return true;
+        }
+    }
 
-				click.active = false;
+    function update(dt) {
+        ball.update(dt);
+    }
 
-				render(ctx);
+    function render(ctx) {
+        drawBall(ctx, ball);
+    }
 
-				oldTime = timestamp;
-				window.requestAnimationFrame(gameLoop);
-			}
+    let oldTime = 0;
+    let count = 0;
+    let click = {
+        active: false,
+        x: 0,
+        y: 0,
+    }
+    function gameLoop(timestamp) {
+        const du = Math.min(timestamp - oldTime, 30);
+        const dt = du / 1000;
 
-			canvas.addEventListener("click", evt=>{
+        ctx.fillStyle = "grey";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-				click.x = evt.offsetX;
-				click.y = evt.offsetY;
+        update(dt);
 
-				ctx.save();
+        click.active = false;
 
-				ctx.fillStyle = "red";
-				ctx.beginPath();
-				ctx.arc(click.x, click.y, 10, 0, Math.PI*2);
-				ctx.closePath();
-				ctx.fill();
-				ctx.restore();
+        render(ctx);
 
+        oldTime = timestamp;
+        window.requestAnimationFrame(gameLoop);
+    }
 
-				click.active = true;
-			})
-			gameLoop("");
-		}
+    canvas.addEventListener("click", evt=>{
+
+        click.x = evt.offsetX;
+        click.y = evt.offsetY;
+
+        ctx.save();
+
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.arc(click.x, click.y, 10, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
+        ctx.restore();
+
+        click.active = true;
+    }
+    )
+    gameLoop("");
+}
